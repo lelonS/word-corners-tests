e4b9f4830e18049efe31357cfd91f3661b8e9c2f
Teddymannen <teddy_kross@hotmail.com>
2023-11-16 17:08:29 +0100

    add tests for 1-letter-placement.js
    Co-Authored-By: lelonS <35070147+lelonS@users.noreply.github.com>

--- cypress/e2e/step_definitions/1-letter-placement.js
+++ cypress/e2e/step_definitions/1-letter-placement.js
@@ -1,6 +1,39 @@
-const { Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+const { Given, When, Then } = require('@badeball/cypress-cucumber-preprocessor');
 
-Given("I visit the game page", () => {
-  cy.visit("/game");
+let boxes = { 'top left': [], 'bottom left': [], 'top right': [], 'bottom right': [] };
+
+
+Given('I am on the game page', () => {
+  boxes = { 'top left': [], 'bottom left': [], 'top right': [], 'bottom right': [] };
+  cy.visit('/game');
+});
+
+Given('the page has loaded', () => {
+  cy.get('.splash').should('not.exist');
+});
+
+When('I place two letters in a row in the {string} box', (boxClass) => {
+  const classes = boxClass.split(' ');
+  for (let i = 0; i < 2; i++) {
+    cy.get(`.middle`).children().should('have.length', 3);
+    cy.get(`.middle`).children().eq(1).then((el) => {
+      boxes[boxClass].push(el.text());
+    });
+
+    cy.get(`.${classes.join('.')}`).click();
+  }
+
+});
+
+Then('the letters should be in the correct order', () => {
+  for (let key in boxes) {
+    const classes = key.split(' ');
+    cy.log(key);
+    let letters = boxes[key].join('');
+    if (key.includes('right')) {
+      letters = letters.split('').reverse().join('');
+    }
+    cy.get(`.${classes.join('.')}`).should('contain', letters); // find better way to check letters
+  }
 });
 
