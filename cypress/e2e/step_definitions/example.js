f19f948c1b89e4d8adc42c38700eac925f6da1a6
lelonS <35070147+lelonS@users.noreply.github.com>
2023-11-16 15:18:51 +0100

    Fix
    Co-Authored-By: Teddymannen <14800686+Teddymannen@users.noreply.github.com>

--- cypress/e2e/step_definitions/example.js
+++ cypress/e2e/step_definitions/example.js
@@ -1,7 +1,11 @@
 const { When, Then } = require("@badeball/cypress-cucumber-preprocessor");
 
 When("I visit the main page", () => {
-  cy.visit("/");
+  cy.visit("/hej", {
+    onBeforeLoad: (win) => {
+      win.console.log = () => { };
+    }
+  });
 });
 
 Then("I should see the text {string}", (text) => {
