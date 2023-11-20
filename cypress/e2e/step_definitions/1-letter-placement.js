f4eb89cab2077b924a270671296fd166f408ca24
lelonS <35070147+lelonS@users.noreply.github.com>
2023-11-20 18:40:03 +0100

    Update 1-letter-placement.js
    Co-Authored-By: Teddymannen <14800686+Teddymannen@users.noreply.github.com>

--- cypress/e2e/step_definitions/1-letter-placement.js
+++ cypress/e2e/step_definitions/1-letter-placement.js
@@ -5,12 +5,32 @@ let boxes = { 'top left': [], 'bottom left': [], 'top right': [], 'bottom right'
 
 Given('I am on the game page', () => {
   boxes = { 'top left': [], 'bottom left': [], 'top right': [], 'bottom right': [] };
+
+  // cy.intercept('GET', '/workers/word-worker.js', (req) => {
+  //   req.continue((res) => {  DOES NOT WORK WINDOW DONT EXIST 
+  //     res.body += ';window.randomChar = randomChar; console.log("randomChar set", randomChar);';
+  //   });
+  // });
+
+  cy.intercept('GET', '/assets/index-d27a4d00.js', (req) => {
+    req.continue((res) => {
+      res.body = res.body.replace('await xt.randomChar()', 'randomChar()');
+      res.body += ';const randomChar = () => {console.log("called");return "Z"}; console.log("randomChar set", randomChar)'
+      // res.body += ';window.exposedProxy = xt; xt.randomChar = () => {console.log("called");return "Z"}; console.log("exposedProxy set", xt)';
+    });
+  });
+
   cy.visit('/game');
 });
 
 Given('the page has loaded', () => {
   cy.wait(1000);
-  cy.get('.splash').should('not.exist');
+  cy.get('.splash', { timeout: 20000 }).should('not.exist');
+
+  // Get window.exposedWorker
+  // cy.window().then((win) => {
+
+  // });
 });
 
 When('I place two letters in a row in the {string} box', (boxClass) => {
