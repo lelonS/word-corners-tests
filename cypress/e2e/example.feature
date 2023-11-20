54547004cdb82625a4352eebd13b6920967cf68e
lelonS <35070147+lelonS@users.noreply.github.com>
2023-11-20 15:04:41 +0100

    Remove example

--- cypress/e2e/example.feature
+++
@@ -1,4 +0,0 @@
-Feature: Go to homepage example
-  Scenario: visiting the main page
-    When I visit the main page
-    Then I should see the text "Word Corners"
--- cypress/e2e/step_definitions/example.js
+++
@@ -1,9 +0,0 @@
-const { When, Then } = require("@badeball/cypress-cucumber-preprocessor");
-
-When("I visit the main page", () => {
-  cy.visit("/");
-});
-
-Then("I should see the text {string}", (text) => {
-  cy.contains(text, { matchCase: false });
-});
